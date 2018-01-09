c880576ff75c8108c73673448541839b9e51cb25
moksu <moksuz43@hotmail.com>
2018-01-09 21:26:56 +0000

    initail commit 2

---
+++ app/src/main/java/com/example/moksu/candmcarpets/Services/DateService.kt
@@ -0,0 +1,36 @@
+package com.example.moksu.candmcarpets.Services
+
+import com.example.moksu.candmcarpets.Model.Category
+import com.example.moksu.candmcarpets.Model.Product
+
+/**
+ * Created by moksu on 09/01/2018.
+ */
+object DateService {
+
+    val categories = listOf(
+            Category ("Carpets", "carpet"),
+            Category("Vinyls", "vinyl"),
+            Category("Laminates", "laminate"),
+            Category("Underlay","underlay")
+
+    )
+
+    val carpets = listOf(
+            Product("", "","")
+
+
+    )
+    val laminates = listOf(
+            Product("", "", "")
+    )
+
+    val vinyls = listOf(
+            Product("","","")
+
+    )
+
+    val underlay = listOf(
+            Product("", "", "")
+    )
+}
